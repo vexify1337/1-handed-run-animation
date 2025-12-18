c98fe55bdd6c0b7477b55f41b7bab043fb704a0f
UE5 <122513007+vexify1337@users.noreply.github.com>
2025-12-18 08:40:50 +0000

    Update fxmanifest.lua

--- s6la_1handed/fxmanifest.lua
+++ s6la_1handed/fxmanifest.lua
@@ -5,8 +5,6 @@ author 's6la'
 description 'One-Handed Weapon Running Animation'
 version '1.0.0'
 
-shared_script '@WaveShield/resource/include.lua'
-shared_script '@WaveShield/resource/waveshield.js'
 shared_scripts {
     '@s6la_bridge/shared/config.lua',
     'shared/config.lua'
@@ -18,4 +16,5 @@ client_scripts {
 
 dependency 's6la_bridge'
 dependency 'ox_lib'
+
 
